7d687464e4c3219f262350a94558f94318d63e4a
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2023-04-25 18:13:13 +0300

    Revert "[K2] don't fail with CCE in case of unresolved imports"
    This reverts commit 9d6afea8d51b6d9cbb483efe1c54436e1a13d6c9.

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/references/FirReferenceResolveHelper.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/references/FirReferenceResolveHelper.kt
@@ -442,8 +442,7 @@ internal object FirReferenceResolveHelper {
             importedFqName = selectedFqName
             isAllUnder = false
         }
-        val resolvedImport = FirImportResolveTransformer(session).transformImport(rawImportForSelectedFqName, null)
-                as? FirResolvedImport ?: return emptyList()
+        val resolvedImport = FirImportResolveTransformer(session).transformImport(rawImportForSelectedFqName, null) as FirResolvedImport
         val scope = FirExplicitSimpleImportingScope(listOf(resolvedImport), session, ScopeSession())
         val selectedName = resolvedImport.importedName ?: return emptyList()
         return buildList {
