16fc26c29d911dcd974b3b158a3b0852dee4831f
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2023-04-26 14:00:10 +0200

    [K2] Force interpretation of default argument for annotation's property
    We miss these expression because they might be represented as
    `IrGetFiled` values, but they still must be turned into `IrConst`.
    
    #KT-58007 Fixed

--- compiler/ir/ir.interpreter/src/org/jetbrains/kotlin/ir/interpreter/checker/IrConstDeclarationAnnotationTransformer.kt
+++ compiler/ir/ir.interpreter/src/org/jetbrains/kotlin/ir/interpreter/checker/IrConstDeclarationAnnotationTransformer.kt
@@ -6,12 +6,15 @@
 package org.jetbrains.kotlin.ir.interpreter.checker
 
 import org.jetbrains.kotlin.constant.EvaluatedConstTracker
+import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.IrStatement
+import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationBase
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.expressions.IrErrorExpression
 import org.jetbrains.kotlin.ir.interpreter.IrInterpreter
+import org.jetbrains.kotlin.ir.util.primaryConstructor
 
 internal class IrConstDeclarationAnnotationTransformer(
     interpreter: IrInterpreter,
@@ -24,6 +27,14 @@ internal class IrConstDeclarationAnnotationTransformer(
 ) : IrConstAnnotationTransformer(interpreter, irFile, mode, evaluatedConstTracker, onWarning, onError, suppressExceptions) {
     override fun visitDeclaration(declaration: IrDeclarationBase, data: Nothing?): IrStatement {
         transformAnnotations(declaration)
+        if (declaration is IrClass && declaration.kind == ClassKind.ANNOTATION_CLASS) {
+            declaration.primaryConstructor?.valueParameters?.forEach {
+                val defaultExpression = it.defaultValue?.expression ?: return@forEach
+                if (defaultExpression.canBeInterpreted()) {
+                    it.defaultValue?.expression = defaultExpression.interpret(failAsError = false)
+                }
+            }
+        }
         return super.visitDeclaration(declaration, data)
     }
 }
--- compiler/testData/codegen/box/involvesIrInterpreter/serialization/annotationWithDefaults.kt
+++ compiler/testData/codegen/box/involvesIrInterpreter/serialization/annotationWithDefaults.kt
@@ -5,6 +5,18 @@
 // MODULE: lib
 // FILE: lib.kt
 
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IntegerNumberValid(
+    val message: String = <!EVALUATED("Has illegal integer number value")!>"Has illegal integer number value"<!>,
+    val groups: Array<KClass<*>> = [],
+
+    val minimum: Long = Long.<!EVALUATED("-9223372036854775808")!>MIN_VALUE<!>,
+    val maximum: Long = Long.<!EVALUATED("9223372036854775807")!>MAX_VALUE<!>
+)
+
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.BINARY)
 annotation class AnnotationWithDefault(val str: String = "Str" <!EVALUATED("String")!>+ "ing"<!>)
