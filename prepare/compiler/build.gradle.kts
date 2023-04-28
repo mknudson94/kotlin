3df2a89ec274edc317b0099fd8b9680f0794c141
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2023-04-28 14:20:22 +0200

    scripting-compiler-plugin-for-ide: publish artifact

--- prepare/compiler/build.gradle.kts
+++ prepare/compiler/build.gradle.kts
@@ -117,7 +117,8 @@ val distCompilerPluginProjects = listOf(
     ":kotlin-sam-with-receiver-compiler-plugin",
     ":kotlinx-serialization-compiler-plugin",
     ":kotlin-lombok-compiler-plugin",
-    ":kotlin-assignment-compiler-plugin"
+    ":kotlin-assignment-compiler-plugin",
+    ":kotlin-scripting-compiler"
 )
 
 val distSourcesProjects = listOfNotNull(
