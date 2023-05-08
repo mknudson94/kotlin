9ab0cb0cfa3477a91b9ed0d2e9b51437365e72f5
Roman Golyshev <roman.golyshev@jetbrains.com>
2023-05-08 15:04:35 +0200

    [AA FIR] KT-58503 Use `resolvedStatus` to access visibility and modality of a class
    Compiler plugins (AllOpen, for example) can modify the status of a
    declaration, including visibility and modality. If we observe them
    without enforcing STATUS resolve, we risk to see incorrect information,
    which in turn can lead to false negatives/positives in
    inspections/intentions.
    
    Add test for light classes. See KT-58503 description for the explanation
    how the initial problem was influencing Spring inspections from intellij
    repository.
    
    ^KT-58503 Fixed

---
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/withTestCompilerPluginEnabled/allOpen.java
@@ -0,0 +1,12 @@
+@org.jetbrains.kotlin.fir.plugin.AllOpen()
+public class BaseClass /* test.BaseClass*/ {
+  private int prop = 0 /* initializer type: int */;
+
+  public  BaseClass();//  .ctor()
+
+  public int getProp();//  getProp()
+
+  public void function();//  function()
+
+  public void setProp(int);//  setProp(int)
+}
