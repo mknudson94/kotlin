26ef22820f85684123a5c9319216b7033410459d
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2023-05-08 14:39:27 +0200

    [k2-evoluator][wip] introduce `compileKt2Ir` to convert KtElement into Ir

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/util/fir2IrConverterUtils.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/util/fir2IrConverterUtils.kt
@@ -5,10 +5,16 @@
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir.util
 
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getFirResolveSession
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getOrBuildFir
+import org.jetbrains.kotlin.analysis.project.structure.getKtModule
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.builtins.DefaultBuiltIns
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.config.LanguageVersionSettings
+import org.jetbrains.kotlin.diagnostics.KtDiagnostic
+import org.jetbrains.kotlin.diagnostics.impl.SimpleDiagnosticsCollectorWithSuppress
 import org.jetbrains.kotlin.fir.FirElement
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.backend.*
@@ -16,26 +22,33 @@ import org.jetbrains.kotlin.fir.backend.generators.AnnotationGenerator
 import org.jetbrains.kotlin.fir.backend.generators.CallAndReferenceGenerator
 import org.jetbrains.kotlin.fir.backend.generators.DelegatedMemberGenerator
 import org.jetbrains.kotlin.fir.backend.generators.FakeOverrideGenerator
+import org.jetbrains.kotlin.fir.backend.jvm.Fir2IrJvmSpecialAnnotationSymbolProvider
+import org.jetbrains.kotlin.fir.backend.jvm.FirJvmKotlinMangler
+import org.jetbrains.kotlin.fir.backend.jvm.FirJvmVisibilityConverter
+import org.jetbrains.kotlin.fir.backend.jvm.JvmFir2IrExtensions
 import org.jetbrains.kotlin.fir.declarations.FirConstructor
 import org.jetbrains.kotlin.fir.declarations.FirFile
 import org.jetbrains.kotlin.fir.declarations.FirRegularClass
 import org.jetbrains.kotlin.fir.descriptors.FirModuleDescriptor
 import org.jetbrains.kotlin.fir.descriptors.FirPackageFragmentDescriptor
 import org.jetbrains.kotlin.fir.expressions.FirFunctionCall
+import org.jetbrains.kotlin.fir.pipeline.runCheckers
+import org.jetbrains.kotlin.fir.pipeline.runResolution
 import org.jetbrains.kotlin.fir.references.toResolvedCallableSymbol
 import org.jetbrains.kotlin.fir.resolve.ScopeSession
 import org.jetbrains.kotlin.fir.resolve.getContainingClass
 import org.jetbrains.kotlin.fir.resolve.toSymbol
 import org.jetbrains.kotlin.fir.symbols.SymbolInternals
 import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirClassifierSymbol
 import org.jetbrains.kotlin.fir.types.ConeLookupTagBasedType
 import org.jetbrains.kotlin.fir.types.FirResolvedTypeRef
 import org.jetbrains.kotlin.fir.visitors.FirVisitorVoid
+import org.jetbrains.kotlin.ir.backend.jvm.serialization.JvmIrMangler
 import org.jetbrains.kotlin.ir.builders.declarations.buildValueParameter
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrFactory
 import org.jetbrains.kotlin.ir.declarations.impl.IrExternalPackageFragmentImpl
+import org.jetbrains.kotlin.ir.declarations.impl.IrFactoryImpl
 import org.jetbrains.kotlin.ir.declarations.impl.IrModuleFragmentImpl
 import org.jetbrains.kotlin.ir.symbols.impl.IrExternalPackageFragmentSymbolImpl
 import org.jetbrains.kotlin.ir.types.impl.IrSimpleTypeImpl
@@ -43,13 +56,14 @@ import org.jetbrains.kotlin.ir.util.IdSignature
 import org.jetbrains.kotlin.ir.util.KotlinMangler
 import org.jetbrains.kotlin.load.kotlin.JvmPackagePartSource
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.psi.KtElement
 
 /**
  * This workaround over [Fir2IrConverter.createModuleFragmentWithSignaturesIfNeeded] to convert cross module FIR to IR.
  * Ideally we should maintain symbol table only, but at current stage we have to fill storages with dependent constructions.
  */
 @OptIn(SymbolInternals::class)
-fun createModuleFragmentWithSignaturesIfNeededWorkaround(
+private fun createModuleFragmentWithSignaturesIfNeededWorkaround(
     session: FirSession,
     scopeSession: ScopeSession,
     firFiles: List<FirFile>,
@@ -165,4 +179,52 @@ fun createModuleFragmentWithSignaturesIfNeededWorkaround(
         false
     )
     return Fir2IrResult(irModuleFragment, components, moduleDescriptor)
+}
+
+fun compileKt2Ir(
+    codeFragment: KtElement,
+    languageVersionSettings: LanguageVersionSettings,
+    project: Project,
+    extensions: JvmFir2IrExtensions,
+    dianosticErrorProcessing: (Map.Entry<String?, List<KtDiagnostic>>) -> Unit
+): Fir2IrResult {
+
+    codeFragment.getKtModule(project)
+
+    val session = codeFragment.getFirResolveSession()
+    val firFile = codeFragment.getOrBuildFir(session)!! as FirFile
+
+    val (scope, firFiles) = session.useSiteFirSession.runResolution(
+        listOf(firFile)
+    )
+    val diagnosticReporter = SimpleDiagnosticsCollectorWithSuppress()
+    session.useSiteFirSession.runCheckers(
+        scope,
+        listOf(firFile),
+        diagnosticReporter
+    )
+    if (diagnosticReporter.hasErrors) {
+
+        diagnosticReporter.diagnosticsByFilePath.forEach(dianosticErrorProcessing)
+    }
+
+    val irGenerationExtension = IrGenerationExtension.getInstances(codeFragment.project)
+
+    val fir2irResult = createModuleFragmentWithSignaturesIfNeededWorkaround(
+        session.useSiteFirSession,
+        scope, firFiles,
+        languageVersionSettings,
+        extensions,
+        JvmIrMangler,
+        IrFactoryImpl, FirJvmVisibilityConverter,
+        Fir2IrJvmSpecialAnnotationSymbolProvider(),
+        irGenerationExtension,
+        false,
+        DefaultBuiltIns.Instance,
+        Fir2IrCommonMemberStorage(true,
+                                  null, //{ JvmIdSignatureDescriptor(JvmDescriptorMangler(null)) },
+                                  { FirJvmKotlinMangler() }),
+        null
+    )
+    return fir2irResult
 }
