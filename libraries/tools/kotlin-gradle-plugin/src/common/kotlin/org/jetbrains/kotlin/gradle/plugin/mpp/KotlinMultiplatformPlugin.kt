d3ec21bc005736f917fb150ac5497be2afe3c2cb
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2023-05-09 12:54:35 +0200

    added mpp platform values into AllowedListAnonymizer for statistics

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/KotlinMultiplatformPlugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/KotlinMultiplatformPlugin.kt
@@ -185,6 +185,7 @@ class KotlinMultiplatformPlugin : Plugin<Project> {
             }
 
             val targetName = if (target is KotlinNativeTarget)
+//                target.targetName
                 target.konanTarget.name
             else
                 target.platformType.name
